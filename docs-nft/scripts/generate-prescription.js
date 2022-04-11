412023627ccddb93eaccca7c9063a2384fa8cf2c
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2022-04-11 12:51:47 +0530

    Prescription Template;

--- docs-nft/scripts/generate-prescription.js
+++ docs-nft/scripts/generate-prescription.js
@@ -5,41 +5,48 @@ var fs = require("fs");
 var html = fs.readFileSync("template.html", "utf8");
 
 var options = {
-    format: "A3",
+    format: "A4",
     orientation: "portrait",
     border: "10mm",
-    header: {
-        height: "45mm",
-        contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
-    },
-    footer: {
-        height: "28mm",
-        contents: {
-            first: 'Cover page',
-            2: 'Second page', // Any page number is working. 1-based index
-            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-            last: 'Last Page'
-        }
-    }
 };
-var users = [
+var meds = [
     {
-        name: "Shyam",
-        age: "26",
+        "name": "trazodone",
+        "for": "Insomnia",
+        "quantity": 2,
+        "comments": "sample generated comment"
     },
     {
-        name: "Navjot",
-        age: "26",
+        "name": "zolpidem",
+        "for": "Insomnia",
+        "quantity": 3,
+        "comments": "sample generated comment"
     },
     {
-        name: "Vitthal",
-        age: "26",
-    },
-];
+        "name": "temazepam",
+        "for": "Insomnia",
+        "quantity": 1,
+        "comments": "sample generated comment"
+    }
+]
+
+var docDetails = {
+    "doctorName": "Dr. Colin Dejesus",
+    "doctorQual": "MBBS, MSC",
+    "doctorSpec": "Psychology",
+}
+
+var patientDetails = {
+    "patientName": "Lillian Toomer",
+    "patientContact": "+91 9650693092"
+}
+
 var document = {
     html: html,
     data: {
-        users: users,
+        meds: meds,
+        docDetails,
+        patientDetails
     },
     path: "./prescriptions/output.pdf",
     type: "",
